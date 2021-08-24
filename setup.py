65f6b73e483e4644e1cf7836aeffa7495f249913
Baris <alyildizb@gmail.com>
2021-08-25 00:21:42 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML pipeline to predict btc price over 24h',
+    author='Baris',
+    license='MIT',
+)
