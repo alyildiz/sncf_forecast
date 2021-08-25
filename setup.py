d5e7b949e98fd41e7260434a181e7d6a2ae65f59
Baris <alyildizb@gmail.com>
2021-08-25 23:32:06 +0200

    remove files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='ML pipeline to predict btc price over 24h',
-    author='Baris',
-    license='MIT',
+    version="0.1.0",
+    description="ML pipeline to predict btc price over 24h",
+    author="Baris",
+    license="MIT",
 )
